d92aac98f4d4da93ff97558d73b3e2b2680c025b
josefphilip <71267682+josefphilip@users.noreply.github.com>
2020-11-05 12:54:58 +0100

    Add project files.

---
+++ Labb5/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Labb5
+{
+    partial class imageCounter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxURL = new System.Windows.Forms.TextBox();
+            this.extractURLBTN = new System.Windows.Forms.Button();
+            this.saveImagesBTN = new System.Windows.Forms.Button();
+            this.labelImagesFound = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.richtextboxURLS = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxURL
+            // 
+            this.textBoxURL.Location = new System.Drawing.Point(12, 12);
+            this.textBoxURL.Name = "textBoxURL";
+            this.textBoxURL.Size = new System.Drawing.Size(695, 20);
+            this.textBoxURL.TabIndex = 0;
+            // 
+            // extractURLBTN
+            // 
+            this.extractURLBTN.Location = new System.Drawing.Point(713, 9);
+            this.extractURLBTN.Name = "extractURLBTN";
+            this.extractURLBTN.Size = new System.Drawing.Size(75, 23);
+            this.extractURLBTN.TabIndex = 1;
+            this.extractURLBTN.Text = "Extract";
+            this.extractURLBTN.UseVisualStyleBackColor = true;
+            this.extractURLBTN.Click += new System.EventHandler(this.extractURLBTN_Click);
+            // 
+            // saveImagesBTN
+            // 
+            this.saveImagesBTN.Location = new System.Drawing.Point(697, 412);
+            this.saveImagesBTN.Name = "saveImagesBTN";
+            this.saveImagesBTN.Size = new System.Drawing.Size(91, 23);
+            this.saveImagesBTN.TabIndex = 3;
+            this.saveImagesBTN.Text = "Save Images";
+            this.saveImagesBTN.UseVisualStyleBackColor = true;
+            this.saveImagesBTN.Click += new System.EventHandler(this.saveImagesBTN_Click);
+            // 
+            // labelImagesFound
+            // 
+            this.labelImagesFound.AutoSize = true;
+            this.labelImagesFound.Location = new System.Drawing.Point(12, 422);
+            this.labelImagesFound.Name = "labelImagesFound";
+            this.labelImagesFound.Size = new System.Drawing.Size(74, 13);
+            this.labelImagesFound.TabIndex = 4;
+            this.labelImagesFound.Text = "Images found:";
+            this.labelImagesFound.Click += new System.EventHandler(this.labelImagesFound_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // richtextboxURLS
+            // 
+            this.richtextboxURLS.Location = new System.Drawing.Point(12, 38);
+            this.richtextboxURLS.Name = "richtextboxURLS";
+            this.richtextboxURLS.Size = new System.Drawing.Size(773, 368);
+            this.richtextboxURLS.TabIndex = 5;
+            this.richtextboxURLS.Text = "";
+            // 
+            // imageCounter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richtextboxURLS);
+            this.Controls.Add(this.labelImagesFound);
+            this.Controls.Add(this.saveImagesBTN);
+            this.Controls.Add(this.extractURLBTN);
+            this.Controls.Add(this.textBoxURL);
+            this.Name = "imageCounter";
+            this.Text = "ImageScraper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxURL;
+        private System.Windows.Forms.Button extractURLBTN;
+        private System.Windows.Forms.Button saveImagesBTN;
+        private System.Windows.Forms.Label labelImagesFound;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.RichTextBox richtextboxURLS;
+    }
+}
+
---
+++ Labb5/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labb5
+{
+    public partial class imageCounter : Form
+    {
+        private readonly List<Task<byte[]>> downloads = new List<Task<byte[]>>();
+        private readonly List<string> imageLinkList = new List<string>();
+        private Task<byte[]> downloadsFinished;
+        private string format = "";
+        private int i;
+        private string path = "";
+
+
+        public imageCounter()
+        {
+            InitializeComponent();
+            if (imageLinkList.Count == 0) saveImagesBTN.Enabled = false;
+            textBoxURL.Text = "https://www.gp.se/";
+        }
+
+        private async void saveImagesBTN_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                if (dialog.ShowDialog() != DialogResult.OK) return;
+                path = dialog.SelectedPath;
+                GetByteArray();
+
+                while (downloads.Count > 0)
+                    for (i = 0; i < imageLinkList.Count; i++)
+                    {
+                        downloadsFinished = await Task.WhenAny(downloads);
+
+                        if (imageLinkList[i].Contains(".png"))
+                        {
+                            format = ".png";
+                            DownloadFiles();
+                        }
+
+                        if (imageLinkList[i].Contains(".bmp"))
+                        {
+                            format = ".bmp";
+                            DownloadFiles();
+                        }
+
+                        if (imageLinkList[i].Contains(".jpg"))
+                        {
+                            format = ".jpg";
+                            DownloadFiles();
+                        }
+
+                        if (imageLinkList[i].Contains(".jpeg"))
+                        {
+                            format = ".jpeg";
+                            DownloadFiles();
+                        }
+
+                        if (imageLinkList[i].Contains(".gif"))
+                        {
+                            format = ".gif";
+                            DownloadFiles();
+                        }
+                        downloads.Remove(downloadsFinished);
+                    }
+            }
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+        }
+
+        private void labelImagesFound_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void extractURLBTN_Click(object sender, EventArgs e)
+        {
+            imageLinkList.Clear();
+            var pattern = "<img.+?src=[\"'](.+?)[\"'].*?>";
+            var html = string.Empty;
+            var url = textBoxURL.Text;
+            if (url == String.Empty || !url.Contains("https://www."))
+            {
+                MessageBox.Show("Please enter a valid input.");
+                return; 
+            }
+            var request = (HttpWebRequest) WebRequest.Create(url);
+            using (var response = (HttpWebResponse) request.GetResponse())
+            using (var stream = response.GetResponseStream())
+            using (var reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            foreach (Match match in Regex.Matches(html, pattern, RegexOptions.IgnoreCase))
+            {
+                var imageLink = match.Groups[1].Value;
+                imageLinkList.Add(textBoxURL.Text + imageLink);
+            }
+
+            richtextboxURLS.Lines = imageLinkList.ToArray();
+            labelImagesFound.Text = $"Images found: {imageLinkList.Count}";
+            if (imageLinkList.Count > 0) saveImagesBTN.Enabled = true;
+        }
+
+        private async Task DownloadFiles()
+        {
+            var fs = new FileStream($"{path}" + "\\" + $"image{i}" + $"{format}", FileMode.Create);
+            await fs.WriteAsync(downloadsFinished.Result, 0, downloadsFinished.Result.Length);
+        }
+
+        private void GetByteArray()
+        {
+            var client = new HttpClient();
+            foreach (var line in richtextboxURLS.Lines) downloads.Add(client.GetByteArrayAsync(line));
+        }
+    }
+}
